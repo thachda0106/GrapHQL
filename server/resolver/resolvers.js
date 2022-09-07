4860d6a80c0933185409b63bc59d2b6bef47df9e
Thach Da <thachbovjp@gmail.com>
2022-09-07 22:22:13 +0700

    mutation, type, schema, resolvers

--- server/resolver/resolvers.js
+++ server/resolver/resolvers.js
@@ -1,8 +1,30 @@
 const { authors, books } = require('../staticData');
 const resolvers = {
+	// query
 	Query: {
 		books: () => books,
-		authors: () => authors
+		book: (parent, args) => {
+			return books.find((book) => book.id == args.id);
+		},
+		authors: () => authors,
+		author: (parent, args) => {
+			return authors.find((author) => author.id == args.id);
+		}
+	},
+	Book: {
+		author: (parent, args) => {
+			return authors.find((author) => author.id == parent.authorId);
+		}
+	},
+	Author: {
+		books: (parent, args) => {
+			return books.filter((book) => book.authorId == parent.id);
+		}
+	},
+	// mutation
+	Mutation: {
+		createAuthor: (parent, args) => args,
+		createBook: (parent, args) => args
 	}
 };
 
