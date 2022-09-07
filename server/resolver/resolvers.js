5781ec6f99ce3f173c120a48984c1a38c69b034d
Thach Da <thachbovjp@gmail.com>
2022-09-07 20:32:28 +0700

    convert staticData.js

--- server/resolver/resolvers.js
+++ server/resolver/resolvers.js
@@ -1,34 +1,8 @@
+const { authors, books } = require('../staticData');
 const resolvers = {
 	Query: {
-		books: () => [
-			{
-				id: 1,
-				name: ' De men phieu luu ky',
-				genre: 'adventure'
-			},
-			{
-				id: 2,
-				name: 'lam giau khong kho',
-				genre: 'education'
-			}
-		],
-		authors: () => [
-			{
-				id: 1,
-				name: 'Ngo Tat',
-				age: 101
-			},
-			{
-				id: 2,
-				name: 'Nam Cao',
-				age: 103
-			},
-			{
-				id: 1,
-				name: 'Vu Trong Phung',
-				age: 106
-			}
-		]
+		books: () => books,
+		authors: () => authors
 	}
 };
 
---
+++ server/staticData.js
@@ -0,0 +1,32 @@
+const books = [
+	{
+		id: 1,
+		name: ' De men phieu luu ky',
+		genre: 'adventure'
+	},
+	{
+		id: 2,
+		name: 'lam giau khong kho',
+		genre: 'education'
+	}
+];
+
+const authors = [
+	{
+		id: 1,
+		name: 'Ngo Tat',
+		age: 101
+	},
+	{
+		id: 2,
+		name: 'Nam Cao',
+		age: 103
+	},
+	{
+		id: 1,
+		name: 'Vu Trong Phung',
+		age: 106
+	}
+];
+
+module.exports = { books, authors };
